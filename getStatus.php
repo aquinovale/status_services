59f262ea956824b7c252159ea01c3714aa9d4903
Vinicius Vale <aquino.vale@example.com>
2015-12-22 21:59:20 -0200

    udpate all

--- getStatus.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	$json = array("re" => true, "status" => 0, "msg" => "without connection", "host" => "viniciusvale");
-
-	/* Output header */
-	header('Content-type: application/json');
-	echo json_encode($json);
--- getStatus.sh
+++ getStatus.sh
@@ -4,7 +4,8 @@
 # Criado por Vinicius Vale - 10/12/2015
 # Pega informação de status do postgres
 ##########
+COMPANY=2 ## Codigo empresa
+SERVICE=1 ## Codigo serviço
 
 HOST=$(hostname)
-#psql -t -c "select '$HOST', 'postgresql', now()::date, now()::time" postgres
-psql -t -c "select '$HOST', 'postgresql', now()" postgres
+psql -t -c "select $COMPANY, '$HOST', $SERVICE, 'running', now()" postgres
